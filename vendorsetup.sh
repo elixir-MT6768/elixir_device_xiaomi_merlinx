ec979c2904baaf2c45bf05e197cab99958f3f5f3
kaikeEXE <kaikepinheio@gmail.com>
2022-12-23 19:34:34 +0000

    merlinx: Add vendorsetup and remove.txt

---
+++ vendorsetup.sh
@@ -0,0 +1,25 @@
+## common tree
+
+git clone -b thirteen  https://github.com/elixir-MT6768/elixir_device_xiaomi_mt6768-common device/xiaomi/mt6768-common
+
+##kernel tree
+
+git clone -b lineage-20 https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
+
+##hardware
+
+git clone -b thirteen https://github.com/elixir-MT6768/elixir_hardware_mediatek hardware/mediatek
+
+##fm radio
+
+git clone -b lineage-20 https://github.com/mt6768-dev/android_packages_apps_MtkFMRadio packages/apps/MtkFMRadio
+
+##mtk sepolicy
+ 
+git clone -b lineage-20 https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+
+##interfaces stuff
+
+git clone -b lineage-20 https://github.com/mt6768-dev/vendor_mediatek_opensource_interfaces vendor/mediatek/opensource/interfaces
+
+git clone -b lineage-20 https://github.com/bengris32/vendor_goodix_opensource_interfaces vendor/goodix/opensource/interfaces
